8b2012a95299f3ff60f10a475f3a4736536cd495
yanke <1062403542@qq.com>
2016-08-25 15:29:36 +0800

    ch4

---
+++ ch4/ch4_12_change_mode.c
@@ -0,0 +1,32 @@
+//test chmod
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    struct stat statbuf;
+    //
+    if (stat("foo", &statbuf) < 0)
+    {
+        printf("stat error for foo\n");
+        exit(1);
+    }
+    if (chmod("foo", (statbuf.st_mode & ~S_IXGRP) | S_ISGID) < 0)
+    {
+        printf("chmod error for foo\n");
+        exit(1);
+    }
+
+    if (chmod("bar", S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH) < 0)
+    {
+        printf("chmod error for bar\n");
+       exit(-1);
+    }
+
+    exit(0);
+    exit(0);
+}
